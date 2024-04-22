6cbe003a73f117221f5369805170bad1c69c578d
JulianaMistron <jumistron@yahoo.com.br>
2024-04-22 18:42:52 -0300

    Add project files.

---
+++ aula6-vetor-vogal/Program.cs
@@ -0,0 +1,19 @@
+﻿char[] caracteres = new char[10] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+Console.WriteLine("Vogais: ");
+
+for (int i = 0; i < 10; i++)
+{
+
+    if (caracteres[i] == 'a' || caracteres[i] == 'e' || caracteres[i] == 'i' || caracteres[i] == 'o' || caracteres[i] == 'u')
+        Console.Write(caracteres[i] + " ");
+}
+Console.WriteLine("\n");
+
+Console.WriteLine("Consoantes: ");
+for (int i = 0; i < 10; i++)
+{
+
+    if (caracteres[i] != 'a' && caracteres[i] != 'e' && caracteres[i] != 'i' && caracteres[i] != 'o' && caracteres[i] != 'u')
+        Console.Write(caracteres[i] + " ");
+}
